dd98000329bbf31d73ef09501281844e4199c481
Wagner Silva <w.silvaaa@hotmail.com>
2016-05-11 11:40:12 -0400

    Atualização do modelo de boleto, conforme alteração ABBC

--- samples/credisis.php
+++ samples/credisis.php
@@ -5,7 +5,7 @@ require '../autoloader.php';
 use OpenBoleto\Agente;
 use OpenBoleto\Banco\Credisis;
 
-$sacado = new Agente('Fernando Maia', '023.434.234-34', 'ABC 302 Bloco N', '72000-000', 'Brasília', 'DF');
+$sacado  = new Agente('Fernando Maia', '023.434.234-34', 'ABC 302 Bloco N', '72000-000', 'Brasília', 'DF');
 $cedente = new Agente('Empresa de cosméticos LTDA', '02.123.123/0001-11', 'CLS 403 Lj 23', '71000-000', 'Brasília', 'DF');
 
 $boleto = new Credisis([
@@ -23,8 +23,8 @@ $boleto = new Credisis([
 	'codigoCooperado'        => '0027',
 	'especieDoc'             => 'DSI',
 	'numeroDocumento'        => '00025',
-//	'layout'                 => 'default-carne.phtml',
-	'instrucoesImpressao'    => FALSE,
+	'layout'                 => 'default-carne.phtml',
+	'instrucoesImpressao'    => false,
 	'descricaoDemonstrativo' => [
 		'Compra de materiais cosméticos',
 		'Compra de alicate',
